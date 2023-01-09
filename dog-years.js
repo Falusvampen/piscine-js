7e317605d84489f31eaf92fe7119b5d608cd4346
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-01-09 12:29:01 +0200

    has to return a int

--- dog-years.js
+++ dog-years.js
@@ -19,21 +19,21 @@
 function dogYears(planet, seconds){
 switch (planet) {
     case 'earth':
-        return ((seconds / 31557600) * 7).toFixed(2);
+        return parseFloat((seconds / 31557600) * 7).toFixed(2);
     case 'mercury':
-      return ((seconds / 0.2408467 / 31557600) * 7).toFixed(2);
+      return parseFloat((seconds / 0.2408467 / 31557600) * 7).toFixed(2);
     case 'venus':
-        return ((seconds / 0.61519726 / 31557600) * 7).toFixed(2);
+        return parseFloat((seconds / 0.61519726 / 31557600) * 7).toFixed(2);
     case 'mars':
-        return ((seconds / 1.8808158 / 31557600) * 7).toFixed(2);
+        return parseFloat((seconds / 1.8808158 / 31557600) * 7).toFixed(2);
     case 'jupiter':
-        return ((seconds / 11.862615 / 31557600) * 7).toFixed(2);
+        return parseFloat((seconds / 11.862615 / 31557600) * 7).toFixed(2);
     case 'saturn':
-        return ((seconds / 29.447498 / 31557600) * 7).toFixed(2);
+        return parseFloat((seconds / 29.447498 / 31557600) * 7).toFixed(2);
     case 'uranus':
-        return ((seconds / 84.016846 / 31557600) * 7).toFixed(2);
+        return parseFloat((seconds / 84.016846 / 31557600) * 7).toFixed(2);
     case 'neptune':
-        return ((seconds / 164.79132 / 31557600) * 7).toFixed(2);
+        return parseFloat((seconds / 164.79132 / 31557600) * 7).toFixed(2);
     default:
         console.log('Please enter a valid planet');
 }
