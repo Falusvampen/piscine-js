3de30556d8fc0fd84dc6189b48e88415ca6a9ebd
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-01-09 12:58:28 +0200

    is this all?

---
+++ biggie-smalls.js
@@ -0,0 +1,7 @@
+// Create 2 variables:
+
+// smalls with the smallest possible number value.
+// biggie with the greatest possible number value.
+
+let smalls = Number.MIN_VALUE;
+let biggie = Number.MAX_VALUE;
