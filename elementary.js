853d5c187c7344918a3933eca7dc77b7b5aa259d
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-01-09 17:35:15 +0200

    works?

--- elementary.js
+++ elementary.js
@@ -4,3 +4,29 @@
 // multiply that acts like the * operator, without using it.
 // divide that acts like the integer division operator /, without using it.
 // modulo that acts like the % operator, without using it.
+
+function multiply(a, b){
+    var c = 0;
+    for (var i = 0; i < b; i++){
+        c += a;
+    }
+    return c;
+}
+
+function divide(a, b) {
+  let count = 0;
+  let total = a;
+  while (total >= b) {
+    total -= b;
+    count++;
+  }
+  return count;
+}
+
+function modulo(a, b) {
+  let total = a;
+  while (total >= b) {
+    total -= b;
+  }
+  return total;
+}
