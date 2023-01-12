53ccd4ffaeab3c18aed427c32df788a53b20a13a
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-01-12 19:08:12 +0200

    nice

---
+++ pyramid.js
@@ -0,0 +1,22 @@
+// Instructions
+// Create a function named pyramid which works just like your triangle function. But, it should be pyramid shaped.
+
+// Output example
+// * character and depth of 5 :
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+function pyramid(char, depth) {
+    res = ''
+    for (let i = 1; i <= depth; i++){
+        if (i !== depth){
+            res += ' '.repeat(depth - i) + char.repeat(2*i-1) + '\n'
+        }else{
+            res += ' '.repeat(depth - i) + char.repeat(2*i-1)
+        }
+    }
+    return res
+}
