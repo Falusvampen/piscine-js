0b2c69bfc5ec03ed5e656d3f2c3a1c9e8d2e5fb7
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-01-12 14:26:09 +0200

    nice

--- Quest2/reverser.js
+++ Quest2/reverser.js
@@ -14,14 +14,14 @@
 function reverse(input) {
 if (Array.isArray(input)){
     let res = [];
-    for (i = input.length -1; i >= 0; i--){
+    for (let i = input.length -1; i >= 0; i--){
     res.push(input[i]);
 };
  return res;
 };
 if (typeof input === "string"){
     let res = "";
-    for (i = input.length-1; i >= 0; i--){
+    for (let i = input.length-1; i >= 0; i--){
     res += input[i]; 
 };
 return res;
