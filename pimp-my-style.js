f9f4a0b6a7efe72de689a789544d53f3a486d9a8
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-01-22 15:41:30 +0200

    cleaned code

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -54,9 +54,8 @@ export function pimp() {
   } else {
     if (click == 14) {
       button.classList.add(styles[click], "unpimp");
-    } else {
-      button.classList.add(styles[click]);
     }
+    button.classList.add(styles[click]);
     click++;
   }
 }
